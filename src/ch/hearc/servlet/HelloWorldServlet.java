0826e6d944284d89d370915fa816e3bfcaa012f9
SCE <sce@gglobaz.ch>
2018-09-07 15:48:48 +0200

    initial commit

--- src/ch/hearc/servlet/HelloWorldServlet.java
+++ src/ch/hearc/servlet/HelloWorldServlet.java
@@ -1,6 +1,8 @@
 package ch.hearc.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.stream.Collectors;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,12 +11,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+
 @WebServlet(
 	name="heloWordlServlet",
 	urlPatterns = "/hello"
 			
 )
 public class HelloWorldServlet extends HttpServlet{
+	
+	Gson gson = new Gson();
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -25,16 +33,43 @@ public class HelloWorldServlet extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		String qui = req.getParameter("qui");
+		Qui qui = gson.fromJson(req.getReader(), Qui.class);
 		
-		req.setAttribute("hellomsg", qui);
+		resp.setContentType("application/json");
 		
-		RequestDispatcher view = req.getRequestDispatcher("helloworld.jsp");
-        view.forward(req, resp);
+		HelloWorld hello = new HelloWorld(qui.qui);
 		
+		String res = gson.toJson(hello);
+		
+		PrintWriter out = resp.getWriter();
+		  
+		out.print(res);
+		
+		out.flush();
 	}
 	
 	
+	class Qui{
+		private String qui;
+		
+		
+		public String getQui() {
+			return this.qui;
+		}
+	}
+	
+	
+	class HelloWorld{
+		private String hello;
+		
+		public HelloWorld(String hello) {
+			this.hello = hello;
+		}
+		
+		public String getHello() {
+			return this.hello;
+		}
+	}
 
 	
 }
